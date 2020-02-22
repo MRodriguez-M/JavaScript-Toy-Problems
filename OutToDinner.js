33c0fa323a1ed8256a44659ed412a54463d6893b
unknown <melissa.rodriguez.murguia@gmail.com>
2020-02-22 13:30:42 -0600

    completed step 2 and finished OutToDinner

--- OutToDinner.js
+++ OutToDinner.js
@@ -6,5 +6,10 @@ Create a variable called tip and assign it the result of multiplying bill by a 1
 Print the total to the JavaScript console.
 */
 
+// Create variables for bill, tip, and total
 var bill = 10.25 + 3.99 + 7.15;
 var tip = bill * 0.15;
+var total = bill + tip;
+
+// Output total cost
+console.log("$" + total);
