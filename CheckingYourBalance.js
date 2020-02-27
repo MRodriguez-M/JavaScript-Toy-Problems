b860ae954840693b2e2acba1e16935f99bdb9a5f
unknown <melissa.rodriguez.murguia@gmail.com>
2020-02-27 14:38:03 -0600

    created if statements for errors in account and finished CheckingYourBalance

--- CheckingYourBalance.js
+++ CheckingYourBalance.js
@@ -14,9 +14,24 @@ var checkBalance = true;
 // If statement to check is balanced will be checked
 if (checkBalance) {
     // If statement to check account is active and blance is greater than zero
-    if (isActive == true && balance > 0) {
+    if (isActive === true && balance > 0) {
         // Print balance
         console.log("Your balance is $" + balance + ".");
+    }
+    // Else if statement to check if account is active
+    else if (isActive === false) {
+        // Print error message if account is not active
+        console.log("Your account is no longer active.");
+    }
+    // Else if statement to check if balance is zero
+    else if (balance === 0) {
+        // Print message notfiying user that account is at zero
+        console.log("Your account is empty.");
+    }
+    // Else if statement to check if balance is less than zero
+    else if (balance < 0) {
+        // Print error message for negative balance
+        console.log("Your balance is negative. Please contact bank.");
     }
 }
 else {
