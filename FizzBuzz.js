b6b5180f180c3eb2e2dd23080c9e86063b699ac3
unknown <melissa.rodriguez.murguia@gmail.com>
2020-03-05 11:54:21 -0600

    created loop for numbers 1-100, print specific word for numbers divisible by 3

---
+++ FizzBuzz.js
@@ -0,0 +1,23 @@
+/*
+Loop through the numbers 1 to 100
+If the number is divisible by 3, print "Fizz"
+If the number is divisible by 5, print "Buzz"
+If the number is divisible by both 3 and 5, print "FizzBuzz"
+If the number is not divisible by 3 or 5, print the number
+*/
+
+var number = 1;
+
+// While loop to check every number from 1-100
+while (number <= 100) {
+    // If statement to check if number is divisible by 3
+    if (number % 3 === 0) {
+        console.log("Fizz");
+    }
+    // Print out number if it does not meet any of the prior requirements
+    else {
+        console.log(number);
+    }
+// Increase number by 1 after every check
+number = number + 1;
+}
