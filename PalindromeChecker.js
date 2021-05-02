04dcdb9df0705e2c3e90366d7c39c0481d0464a5
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-05-02 14:01:02 -0500

    Added code to check if single words are palindromes

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -4,4 +4,13 @@ Return true if the given string is a palindrome. Otherwise, return false.
 Remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
 */
 function palindrome(str) {
+  //Create reverse string to compare against original
+  var reverseStr = str.split("").reverse().join("");
+
+  //Compare original and reversed strings
+  if (str == reverseStr) {
+    return true;
+  } else {
+    return false;
   }
+}
