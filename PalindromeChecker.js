4e11b37759bb9a3ba4a18395ad54cb5d1d81adc8
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-05-05 16:08:57 -0500

    Created edited string and added methods to make a more accurate palindrome comparison

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -4,11 +4,13 @@ Return true if the given string is a palindrome. Otherwise, return false.
 Remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
 */
 function palindrome(str) {
-  //Create reverse string to compare against original
-  var reverseStr = str.split("").reverse().join("");
+  //Create new version of original string with no punctuation, spaces, or symbols
+  var cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  //Create reverse string to compare against clean version of original string
+  var reverseStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase().split("").reverse().join("");
 
   //Compare original and reversed strings
-  if (str == reverseStr) {
+  if (cleanStr == reverseStr) {
     return true;
   } else {
     return false;
