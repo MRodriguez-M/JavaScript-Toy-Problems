0236ca6cf7d8d66bd452fedb3d3c7307c6da63b6
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-05-06 02:27:53 -0500

    Implemented code to create roman numerals for thousands place or higher

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -4,5 +4,18 @@ Convert a given number into a roman numeral.
 All roman numeral answers should be provided in upper-case.
 */
 function romanNumeral(num) {
+    //Convert parameter to array of string digits
+    var numArr = num.toString().split("");
+    var romanNumeral = "";
+    var thousandsNum = "";
     
+    //Check if number has thousands place or greater
+    if (numArr.length >= 4) {
+        //Loop to get number of thousands
+        for(var i = 0; i < numArr.length - 3; i++) {
+            thousandsNum += numArr[i];
+        }
+    }
+    romanNumeral = "M".repeat(thousandsNum);
+    return romanNumeral;
 }
