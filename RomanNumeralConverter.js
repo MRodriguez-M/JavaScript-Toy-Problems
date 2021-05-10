8bdc820d7c2d6d58d88407c71c2904763bd10962
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-05-09 23:38:36 -0500

    Add switch cases for roman numerals of tens place

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -51,5 +51,37 @@ function romanNumeral(num) {
             break;
     }
 
+    //Switch cases to assign roman numeral value based on tens place digit
+    switch (numArr[numArr.length - 2]) {
+        case "1":
+            romanNumeral += "X";
+            break;
+        case "2":
+            romanNumeral += "XX"
+            break;
+        case "3":
+            romanNumeral += "XXX";
+            break;
+        case "4":
+            romanNumeral += "XL";
+            break;
+        case "5":
+            romanNumeral += "L";
+            break;
+        case "6":
+            romanNumeral += "LX";
+            break;
+        case "7":
+            romanNumeral += "LXX";
+            break;
+        case "8":
+            romanNumeral += "LXXX";
+            break;
+        case "9":
+            romanNumeral += "XC";
+            break;
+        default:
+            break;
+    }
     return romanNumeral;
 }
