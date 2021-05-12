7e70bcf350a8bfa3a24d362308449fad32f496ac
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-05-11 19:46:35 -0500

    Add instructions and set up

---
+++ ShiftCipher.js
@@ -0,0 +1,7 @@
+/*
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+*/
+function rot13(str) {
+}
