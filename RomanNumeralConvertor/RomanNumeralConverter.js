2710e358c5e5c7358d8828d68a21a8118dbb9806
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-07-15 23:05:30 -0600

    Re-doing problem by applying ES6

---
+++ RomanNumeralConvertor/RomanNumeralConverter.js
@@ -0,0 +1,11 @@
+/*
+Convert a given number into a roman numeral.
+
+All roman numeral answers should be provided in upper-case.
+*/
+
+const romanNumeralConvertor = (num) => {
+    return num;
+}
+
+module.exports = romanNumeralConvertor;
