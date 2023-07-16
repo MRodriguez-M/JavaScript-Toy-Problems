e1bddcee350c2aadaafa3d9dcca645bd5b6c3245
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-07-15 22:04:16 -0600

    Re-doing toy problem applying ES6

---
+++ PalindromeChecker/PalindromeChecker.js
@@ -0,0 +1,11 @@
+/*
+Return true if the given string is a palindrome. Otherwise, return false.
+
+Remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+*/
+
+const palindromeChecker = (str) => {
+    return str;
+}
+
+module.exports = palindromeChecker;
