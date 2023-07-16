e3fc8a1c057a61c56c335db704aaba09b7e2088d
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-07-15 23:05:48 -0600

    Set up tests

---
+++ RomanNumeralConvertor/RomanNumeralConvertor.test.js
@@ -0,0 +1,7 @@
+const romanNumeralConvertor = require('./RomanNumeralConverter');
+
+describe("Roman Numeral Convertor", () => {
+    test("2 returns correct roman numeral", () => {
+        let result = romanNumeralConvertor(2);
+    });
+})
