6a060cf22ab7adef924c14018672e54c18275288
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-07-15 22:38:07 -0600

    Added checks, all pass

--- PalindromeChecker/PalindromeChecker.test.js
+++ PalindromeChecker/PalindromeChecker.test.js
@@ -3,17 +3,25 @@ const palindromeChecker = require('./PalindromeChecker');
 describe("Palindrome Checker Tests", () => {
     test("Return value should be a boolean", () => {
         let result = palindromeChecker("eye");
+
+        expect(result).toEqual(expect.any(Boolean));
     });
 
     test("Palindrome string returns true", () => {
         let result = palindromeChecker("eye");
+
+        expect(result).toBeTruthy();
     });
 
     test("Palindrome string with non-alphanumeric characters returns true", () => {
         let result = palindromeChecker("A man, a plan, a canal. Panama");
+
+        expect(result).toBeTruthy();
     });
 
     test("Non-palindrome string returns false", () => {
         let result = palindromeChecker("not a palindrome");
+
+        expect(result).toBeFalsy();
     });
 })
