5c60c4d7a6c181fff05404d9f81d1a12a2d38c54
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-07-15 22:24:10 -0600

    Rewrote functional code

--- PalindromeChecker/PalindromeChecker.js
+++ PalindromeChecker/PalindromeChecker.js
@@ -5,7 +5,18 @@ Remove all non-alphanumeric characters (punctuation, spaces and symbols) and tur
 */
 
 const palindromeChecker = (str) => {
-    return str;
+    //Create new version of original string with no punctuation, spaces, or symbols
+    let cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    //Create reverse string to compare against clean version of original string
+    let reverseStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase().split("").reverse().join("");
+
+    //Compare original and reversed strings
+    if(cleanStr == reverseStr) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 module.exports = palindromeChecker;
