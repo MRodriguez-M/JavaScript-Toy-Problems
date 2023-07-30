d6ebee2c81d1405b46064ae84c8558aa4e50f73b
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-07-29 21:47:01 -0600

    Re-doing toy problem with ES6

---
+++ ShiftCipher/ShiftCipher.js
@@ -0,0 +1,9 @@
+/*
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+*/
+
+const shiftCipher = (str) => {
+    return str;
+}
