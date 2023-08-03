3f280f1e2cde59b3781122c72df333828c4e4129
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-08-03 13:56:16 -0600

    Rewrote if conditionals

--- ShiftCipher/ShiftCipher.js
+++ ShiftCipher/ShiftCipher.js
@@ -5,5 +5,24 @@ All letters will be uppercase. Do not transform any non-alphabetic character (i.
 */
 
 const shiftCipher = (str) => {
-    return str;
+    let encodeStr = "";
+
+    for(let i = 0; i < str.length; i++) {
+        //Check and encode for letters A-M
+        if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 77) {
+            encodeStr += String.fromCharCode(str.charCodeAt(i) + 13);
+        }
+        //Check and encode for letters N-Z
+        else if(str.charCodeAt(i) >= 78 && str.charCodeAt(i) <= 90) {
+            encodeStr += String.fromCharCode(str.charCodeAt(i) - 13);
+        }
+        //Check for characters not A-Z and push as is
+        else {
+            encodeStr += str[i];
+        }
+    }
+
+    return encodeStr;
 }
+
+module.exports = shiftCipher;
